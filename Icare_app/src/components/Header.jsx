6ce92cec536b97ade41f497c528bc530e96e9003
KasiaRaven26 <rejven777@gmail.com>
2024-01-05 20:38:50 +0100

    logo commit

--- Icare_app/src/components/Header.jsx
+++ Icare_app/src/components/Header.jsx
@@ -1,9 +1,11 @@
-import React from "react";
+import logo from "./I_care.jpg";
 import "./Header.css";
 
-export function Header(props) {
+export function Header() {
   return (
     <div>
+      <img className="logo" src={logo} alt="Logo" />
+
       <h3>Icare</h3>
     </div>
   );
