f2d229aad38b4eb96c10b42cee7b1475db6e7431
Adam Kirwan <ajkirwan1@gmail.com>
2024-01-08 20:11:52 +0000

    Added Modal

--- Icare_app/src/components/HomePage/HomePage.jsx
+++ Icare_app/src/components/HomePage/HomePage.jsx
@@ -4,14 +4,23 @@ import { Header } from "../Header";
 import { Footer } from "../Footer";
 import carer from "../../components/ContetItems/carer.jpg"
 import { useState } from "react";
+import { Modal } from "../Modal/Modal";
+import { ModalContent } from "../Modal/ModalContent";
 
 function HomePage() {
     const [showButtons, setShowButtons] = useState(true)
+    const [showModal, setShowModal] = useState(false)
 
     const buttonTrigger = (result) => {
       setShowButtons(result);
       console.log(result)
-      
+    }
+
+    const ShowTheModalButton = () => {
+      setShowModal(true)
+    }
+    const HideTheModalButton = (result) => {
+      setShowModal(result)
     }
 
   const myStyle = {
@@ -22,6 +31,9 @@ function HomePage() {
 };
   return (
     <div style={myStyle} >
+      <div>
+      {showModal ? <ModalContent HidingTheButton={HideTheModalButton}/> : null}
+      </div>
       <div className="Header">
         <Header />
       </div>
@@ -30,6 +42,7 @@ function HomePage() {
       </div>
       <div className="footer">
         <Footer anotherResult={buttonTrigger}/>
+        <button onClick={ShowTheModalButton}>Show Modal</button>
       </div>
     </div>
   );
