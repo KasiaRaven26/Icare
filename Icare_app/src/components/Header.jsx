14441e56bf6241be677d65d48328ec071c0a0f49
ajkirwan1 <ajkirwan1@gmail.com>
2024-01-11 17:19:55 +0000

    Some things (#8)

--- Icare_app/src/components/Header.jsx
+++ Icare_app/src/components/Header.jsx
@@ -3,10 +3,16 @@ import logo from "./I_care.jpg";
 
 export function Header() {
   return (
-    <>
+    <div>
       <img className="logo" src={logo} alt="Logo" />
-
-      <a href="https://www.w3schools.com">UK/PL</a>
-    </>
+      {/*
+      1 - The buttons need some styling and positioning in the header bar. Add a class name to them, e.g., className = "login-button", class="change-languge button".
+      You'll then need to add the classname to the Header.css file, e.g., .login-button { height: 50px, etc..... }  
+      2 - Add an 'onClick' to the buttons. For instance, the buttons should look something like <button onclick={clickEvent}>Login</button>
+      3 - Above the return statement above, write an arrow function for the clickEvent. Something like - const clickEvent = () => { console.log("Hello Kasia")}
+    */}
+      <button>Login</button>
+      <button>Change Language</button>
+    </div>
   );
 }
