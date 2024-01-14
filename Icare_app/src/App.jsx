d326ca61e51a79ddedaa482d927abed439ecf7a5
Adam Kirwan <ajkirwan1@gmail.com>
2024-01-14 13:46:18 +0000

    Layout

--- Icare_app/src/App.jsx
+++ Icare_app/src/App.jsx
@@ -1,16 +1,18 @@
-import ReactDOM from "react-dom/client"
-import { BrowserRouter, Routes, Route } from "react-router-dom";
-import HomePage from "./components/HomePage/HomePage";
-import { Modal } from "./components/Modal/Modal";
-
+// import ReactDOM from "react-dom/client"
+// import { BrowserRouter, Routes, Route } from "react-router-dom";
+// import HomePage from "./components/HomePage/HomePage";
+// import { Modal } from "./components/Modal/Modal";
+import "./App.css";
 
 function App() {
   return (
-    <div>
-      <div>Header</div>
-      <div>SideBar</div>
-      <div>Content</div>
-      <div>Footer</div>
+    <div className="container">
+      <div className="header">Header</div>
+      <div className="side-bar-content-container">
+        <div className="side-bar">SideBar</div>
+        <div className="content">Content</div>
+      </div>
+      <div className="footer">Footer</div>
     </div>
     // <BrowserRouter>
     //   <Routes>
