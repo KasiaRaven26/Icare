30810d84f77e745aa155b7f899887f351e6c67ca
Adam Kirwan <ajkirwan1@gmail.com>
2024-01-18 20:44:49 +0000

    Icons added

--- Icare_app/src/components/Header.jsx
+++ Icare_app/src/components/Header.jsx
@@ -1,17 +1,21 @@
 import React from "react";
 import logo from "./I_care.jpg";
-import './Header.css';
+import "./Header.css";
+import { FaRegMessage } from "react-icons/fa6";
+import { RiAccountBoxLine } from "react-icons/ri";
 
 export function Header() {
   return (
-    <div style={{"position" : "relative"}}>
+    <div style={{ position: "relative" }}>
       <div className="image-div">
         <img className="logo" src={logo} alt="Logo" />
       </div>
       <div className="link-div">
-        <a  href="./client">Privacy</a>
+        {/* <a  href="./client">Privacy</a>
         <a href="./client">Login</a>
-        <a  href="./client">About Us</a>
+        <a  href="./client">About Us</a> */}
+        <FaRegMessage style={{"margin" : "8 40 0 0"}} size={50}></FaRegMessage>
+        <RiAccountBoxLine size={60}/>
       </div>
     </div>
   );
