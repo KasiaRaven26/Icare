252a5bbef15be5927e5153d85b4c244104a81308
KasiaRaven26 <rejven777@gmail.com>
2024-01-19 21:02:31 +0100

    edited icons

--- Icare_app/src/components/Header.jsx
+++ Icare_app/src/components/Header.jsx
@@ -1,17 +1,15 @@
 import React from "react";
 import logo from "./I_care.jpg";
-import './Header.css';
+import "./Header.css";
 
 export function Header() {
   return (
-    <div style={{"position" : "relative"}}>
+    <div style={{ position: "relative" }}>
       <div className="image-div">
         <img className="logo" src={logo} alt="Logo" />
       </div>
       <div className="link-div">
-        <a  href="./client">Privacy</a>
         <a href="./client">Login</a>
-        <a  href="./client">About Us</a>
       </div>
     </div>
   );
