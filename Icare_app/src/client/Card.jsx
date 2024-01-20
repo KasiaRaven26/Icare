395c422b39218c8976a543235ff2163655f852a4
Adam Kirwan <ajkirwan1@gmail.com>
2024-01-20 14:34:49 +0000

    More icons

--- Icare_app/src/client/Card.jsx
+++ Icare_app/src/client/Card.jsx
@@ -1,6 +1,7 @@
 // import React from "react";
 import { Button } from "../components/ContetItems/Button";
 import { GoChevronLeft } from "react-icons/go";
+import { MdQuestionMark } from "react-icons/md";
 
 import styles from "./Card.module.css";
 
@@ -13,6 +14,14 @@ const Card = (props) => {
       <div className={styles.content}>
         <div className={styles.iconDiv}>
           <GoChevronLeft size={30} />
+        </div>
+        <div className={styles.questionMark}>
+          <div style={{position: "relative"}}>
+            <span className={styles.tooltiptext}>
+              Here is some information about the tile
+            </span>
+            <MdQuestionMark size={20} />
+          </div>
         </div>
       </div>
     </div>
