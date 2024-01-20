674e53972d6e54dc14bd2077c0fa8636fcd824d9
Adam Kirwan <ajkirwan1@gmail.com>
2024-01-20 13:51:57 +0000

    More icons

--- Icare_app/src/client/Card.jsx
+++ Icare_app/src/client/Card.jsx
@@ -1,13 +1,20 @@
 // import React from "react";
 import { Button } from "../components/ContetItems/Button";
+import { GoChevronLeft } from "react-icons/go";
 
 import styles from "./Card.module.css";
 
 const Card = (props) => {
   return (
     <div className={styles.card}>
-      {/* <h1 className="h1"><a className="a">{props.children}</a></h1> */}
-      <h1 className={styles.header}><a className={styles.link}>{props.children}</a></h1>
+      <h1 className={styles.header}>
+        <a className={styles.link}>{props.children}</a>
+      </h1>
+      <div className={styles.content}>
+        <div className={styles.iconDiv}>
+          <GoChevronLeft size={30} />
+        </div>
+      </div>
     </div>
   );
 };
