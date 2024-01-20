a6790a7b68b87f14a25cf5b84d943b0c998a6a30
KasiaRaven26 <144065101+KasiaRaven26@users.noreply.github.com>
2024-01-20 20:56:45 +0100

    Header fixes (#24)
    * edited-modals
    
    * logo-changed

--- Icare_app/src/client/ClientHeader.jsx
+++ Icare_app/src/client/ClientHeader.jsx
@@ -1,19 +1,21 @@
 import logo from "../components/I_care.jpg";
-import styles from "./ClientHeader.module.css";
+import styles from "./ClientHeader.module.css"
 import { FaRegMessage } from "react-icons/fa6";
 import { RiAccountBoxLine } from "react-icons/ri";
-import { FaSearch } from "react-icons/fa";
+
 
 const ClientHeader = () => {
   return (
     <div style={{ position: "relative" }}>
-      <div className={styles.imageDiv}>
-        <img className="logo" src={logo} alt="Logo" />
+     <div className={styles.imageDiv}>
+<a href="/">  <img className="logo" src={logo} alt="Logo" /> </a>
+
+
+       
       </div>
       <div className={styles.linkDiv}>
-        <FaSearch style={{ margin: "8 40 0 0" }} size={50}/>
-        <FaRegMessage style={{ margin: "8 40 0 0" }} size={50}></FaRegMessage>
-        <RiAccountBoxLine size={60} />
+        <FaRegMessage style={{"margin" : "8 40 0 0"}} size={50}></FaRegMessage>
+        <RiAccountBoxLine size={60}/>
       </div>
     </div>
   );
