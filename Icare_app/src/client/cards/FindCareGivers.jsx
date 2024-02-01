cfb0cdea97bc4dfdeab293641a1ba48fe097075e
KasiaRaven26 <rejven777@gmail.com>
2024-02-01 21:05:01 +0100

    tables

--- Icare_app/src/client/cards/FindCareGivers.jsx
+++ Icare_app/src/client/cards/FindCareGivers.jsx
@@ -1,8 +1,53 @@
 import Card from "../Card"
+import styles from "./FindCareGivers.module.css"
+
 
 const FindCareGivers = () => {
     return (
-        <Card>Find Care Givers</Card>
+        <Card> 
+           <table>
+                 <thead>
+                    <tr>
+                        <th>name</th> 
+                        <th>age</th>
+                        <th>location</th>
+                        <th>experience</th>
+                        <th>availability</th>
+                    </tr>
+                 </thead> 
+                 <tbody>
+                    <tr>
+                        <td>Margharet Kirwan</td>
+                        <td>60</td>
+                        <td>Poole</td>
+                        <td>5 years</td>
+                        <td>immediately</td>
+                    </tr>
+                    <tr>
+                        <td>Magdalena Kruk</td>
+                        <td>68</td>
+                        <td>Warsaw</td>
+                        <td>3 years</td>
+                        <td>immediately</td>
+                    </tr>
+                    <tr>
+                        <td>Adam Kirwan</td>
+                        <td>31</td>
+                        <td>Bristol</td>
+                        <td>1 year</td>
+                        <td>immediately</td>
+                    </tr>
+                     <tr>
+                        <td>Kasia Kruk</td>
+                        <td>26</td>
+                        <td>Amsterdam</td>
+                        <td>2 years</td>
+                        <td>never</td>
+                    </tr>
+                 </tbody> 
+             </table>
+         </Card>
+        
     )
 }
 
