05b68b2758d3c4ca1a6b8981786db30e3bd44e3e
ajkirwan1 <ajkirwan1@gmail.com>
2024-03-07 11:23:57 +0000

    client-home-page-cards (#43)

--- Icare_app/src/client/ClientContent.jsx
+++ Icare_app/src/client/ClientContent.jsx
@@ -1,48 +1,31 @@
 import { useState } from "react";
 import "./ClientContent.css";
-import Card from "./Card";
 import ClientSideBar from "./ClientSideBar";
-import PersonalDetailsCard from "./pages/personal-details/PersonalDetailsCard";
-import FindCaregiversCard from "./pages/homepage/FindCaregiversCard";
 import YourMessagesCard from "./pages/homepage/YourMessagesCard";
 import ResumeCard from "./pages/homepage/ResumeCard";
-
-
+import AvailableJobsCard from "./pages/homepage/AvailableJobsCard";
+import YourCurrentCareGiver from "./pages/homepage/YourCurrentCareGiver";
 
 const ClientContent = () => {
   const [sidebarOpen, setSideBarOpen] = useState(false);
-  const [toggleCards, setToggeleCards] = useState(false);
 
   const handleSidebar = () => {
     setSideBarOpen(!sidebarOpen);
   };
 
-  const handleToggleCards = () => {
-    setToggeleCards(!toggleCards);
-  };
-
   return (
     <div className="content-grid-container">
       <div onClick={handleSidebar} className="client-side-bar">
-      <ClientSideBar sideBarOpen={sidebarOpen}/>
-
+        <ClientSideBar sideBarOpen={sidebarOpen} />
       </div>
-      
+
       <div className="card-container">
         <div className="card-container-top">
-          <div
-            onClick={handleToggleCards}
-            className={
-              toggleCards ? "personal-details-open" : "personal-details-closed"
-            }
-          >
-            <PersonalDetailsCard> </PersonalDetailsCard>
+          <div className="my-messages">
+            <AvailableJobsCard />
           </div>
-          <div
-            onClick={handleToggleCards}
-            className={toggleCards ? "cover-letter-closed" : "cover-letter-open"}
-          >
-          <FindCaregiversCard></FindCaregiversCard>
+          <div className="my-messages">
+            <YourCurrentCareGiver />
           </div>
         </div>
 
