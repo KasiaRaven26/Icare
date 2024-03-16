bfac4627788ee012e2361a74eeed8c81cb1dcb36
Adam Kirwan <ajkirwan1@gmail.com>
2024-03-16 18:34:27 +0000

    commit

--- Icare_app/src/client/pages/homepage/YourInbox.jsx
+++ Icare_app/src/client/pages/homepage/YourInbox.jsx
@@ -10,16 +10,19 @@ const YourInbox = () => {
       <h1 className={styles.header}>your inbox</h1>
       <div className={styles.content}>
         <div>
-          <div className={styles.clientsmessages}>
-            Hi Samantha,I would like to disscuss a job offer with you!Are you
-            available?
+          <div className={styles.imageAndMessage}>
+            <img className={styles.smallLogo} src={image}></img>
+            <div className={styles.clientsmessages}>
+              Hi Samantha,I would like to disscuss a job offer with you!Are you
+              available?
+            </div>
           </div>
           <div className={styles.caregiversmessages}>
             Yes sure!When would it suit you best to have a conversation?Best
             regards,Samantha.
           </div>
           <div className={styles.clientsmessages}>
-            Let's say 4PM UK time on Saturday?
+            Lets say 4PM UK time on Saturday?
           </div>
           <div className={styles.clientsmessages}>
             Oh sorry!Can we make it 5PM instead?
